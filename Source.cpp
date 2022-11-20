f787dcadd93af916df553fe483f27ac93bf0e9ce
LRomVik <118601517+LRomVik@users.noreply.github.com>
2022-11-20 12:58:52 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+bool isEven()
+{
+	int a;
+	std::cout << "Enter a number" << std::endl;
+	std::cin >> a;
+	if (a % 2 == 0) {
+		std::cout << "True" << std::endl;
+		return true;
+	}
+	else {
+		std::cout << "False" << std::endl;
+		return false;
+	}
+}
+int main()
+{
+	isEven();
+	return 0;
+}
