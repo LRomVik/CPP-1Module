3d884a8d32ef0f2f6594769c2117bcdc32a54b8f
codik <5ochta@bk.ru>
2022-11-21 12:14:08 +0700

    About vector

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,32 @@
 //Версия от codik754
+//Это про вектор
 
-#include<iostream>
-#include "example.h"
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 //Это комментарий
 
-bool isEven()
-{
-	int a;
-	std::cout << "Enter a number" << std::endl;
-	std::cin >> a;
-	if (a % 2 == 0) {
-		std::cout << "True" << std::endl;
-		return true;
-	}
-	else {
-		std::cout << "False" << std::endl;
-		return false;
-	}
-}
-
-void fun(){
-   cout << "Простая функция" << endl;
-}
 
 int main()
 {
-	isEven();
-   fun();
+	vector<int> v;
+   v.push_back(5);
+   v.push_back(8);
+   v.push_back(3);
+   v.push_back(4);
+   v.push_back(9);
+
+   for(size_t i = 0; i < v.size(); ++i){
+      cout << v[i] << endl;
+   }
+   
+   cout << endl << "This is vector with 99: " << endl;
+   v.push_back(99); 
+   for(auto &it : v){
+      cout << it << endl;
+   }
+
 	return 0;
 }
